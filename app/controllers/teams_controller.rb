53638ba0c978ca017c0adf461efbeb51eeb06b52
YOUR NAME <YOUR@EMAIL.com>
2018-02-06 10:36:53 -0200

    atualizacao

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,11 +2,6 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy, :rota]
 
 
-  before_action :user_not_logged, except: [:new, :create]
-  before_action :logged_user, only: [:new, :create]
-  before_action :right_user_or_admin, only: [:edit, :update, :destroy]
-  before_action :correct_user, only: [:update]
-
   # GET /teams
   # GET /teams.json
   def index
